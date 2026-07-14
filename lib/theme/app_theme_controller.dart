274bf461db9370480dd9056605f3e753feb88ddf
Linda5-umwali <l.umwali@alustudent.com>
2026-07-14 15:15:42 +0200

    added a theme controller for dark and light mode

---
+++ lib/theme/app_theme_controller.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AppThemeController extends ChangeNotifier {
+  AppThemeController._();
+
+  static final AppThemeController instance = AppThemeController._();
+
+  static const String _themeKey = 'app_theme_mode';
+
+  ThemeMode _themeMode = ThemeMode.system;
+
+  ThemeMode get themeMode => _themeMode;
+
+  Future<void> load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final savedMode = prefs.getString(_themeKey);
+
+    _themeMode = ThemeMode.values.firstWhere(
+      (mode) => mode.name == savedMode,
+      orElse: () => ThemeMode.system,
+    );
+
+    notifyListeners();
+  }
+
+  Future<void> setThemeMode(ThemeMode mode) async {
+    _themeMode = mode;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString(_themeKey, mode.name);
+    notifyListeners();
+  }
+}
